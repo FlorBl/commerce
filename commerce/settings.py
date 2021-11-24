1e19516fe78ec3d51e3454f9e1fa405873565de4
Florian <FlorBl>
2021-11-24 17:13:00 -0500

    ///

--- commerce/settings.py
+++ commerce/settings.py
@@ -85,6 +85,12 @@ DATABASES = {
     }
 }
 
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
+
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+MEDIA_URL = '/media/'
+
 AUTH_USER_MODEL = 'auctions.User'
 
 # Password validation
